dc26d430a23796405a69529f5c09f576c65c961d
TomZanna <43095563+TomZanna@users.noreply.github.com>
2025-08-27 15:44:39 +0200

    1.9

---
+++ src/1-getting_started/1.9-coordinate-systems/ex2/xmake.lua
@@ -0,0 +1,6 @@
+target("1.9-coordinate-systems-ex2")
+    set_kind("binary")
+    add_rules("utils.bin2c", {extensions = {".fs", ".vs"}})
+    add_files("shader.*")
+    add_files("main.cpp")
+    add_packages("glfw", "glad", "stb", "glm")
