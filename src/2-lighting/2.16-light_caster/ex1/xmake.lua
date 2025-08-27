d7b4de01251cd5c3f67492c3da96f65136a5aeff
TomZanna <43095563+TomZanna@users.noreply.github.com>
2025-08-27 15:46:11 +0200

    2.16

---
+++ src/2-lighting/2.16-light_caster/ex1/xmake.lua
@@ -0,0 +1,6 @@
+target("2.16-light_caster-ex1")
+    set_kind("binary")
+    add_rules("utils.bin2c", {extensions = {".fs", ".vs"}})
+    add_files("*.vs", "*.fs")
+    add_files("main.cpp")
+    add_packages("glfw", "glad", "stb", "glm")
