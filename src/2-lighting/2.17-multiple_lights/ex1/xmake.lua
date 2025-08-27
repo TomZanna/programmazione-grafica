abb9c9dee508b196c2f75ea3ee404512147c9b68
TomZanna <43095563+TomZanna@users.noreply.github.com>
2025-08-27 15:46:21 +0200

    2.17

---
+++ src/2-lighting/2.17-multiple_lights/ex1/xmake.lua
@@ -0,0 +1,6 @@
+target("2.17-multiple_lights-ex1")
+    set_kind("binary")
+    add_rules("utils.bin2c", {extensions = {".fs", ".vs"}})
+    add_files("*.vs", "*.fs")
+    add_files("main.cpp")
+    add_packages("glfw", "glad", "stb", "glm")
